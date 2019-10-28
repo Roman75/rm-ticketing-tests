ce9d5f96e4cbf5f52da000fdc2a83cfbb7a3552e
Roman Marlovits <roman.marlovits@gmail.com>
2019-10-28 21:34:48 +0100

    good working setup example for dev environment

--- src/www/basic-dev-environment/src/app.js
+++ src/www/basic-dev-environment/src/app.js
@@ -1,5 +1,4 @@
 import readingTime from "reading-time";
-import $ from "jquery";
 import page from "page";
 
 window.calcRT = ev => {
@@ -7,23 +6,28 @@ window.calcRT = ev => {
 	document.getElementById("readingTime").innerText = stats;
 };
 
-$(document).ready(function() {
+window.setTimeout(() => {
+
 	console.log("ready!");
 
-	page.base('/basic-dev-environment');
+	page.base('');
 
 	page('/', (res) => {
+		console.log('0 ============');
 		console.log('root', res);
 	});
 	page('/about', (res) => {
+		console.log('1 ============');
 		console.log('about', res);
 	});
 	page('/contact', (res) => {
+		console.log('3 ============');
 		console.log('contact', res);
 	});
 	page('/contact/:contactName', (res) => {
+		console.log('4 ============');
 		console.log('contact with param?', res);
 	});
 	page();
 
-});
+}, 1000);
