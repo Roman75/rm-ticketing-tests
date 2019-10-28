b9c07bd429b213ddb6d812d01cdb0aee2b89db17
Roman Marlovits <roman.marlovits@gmail.com>
2019-10-29 00:37:29 +0100

    uglify included in build process with package.json => works nice

--- dev.sh
+++ dev.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-rm dist/*
 cp src/index.html dist/index.html
 cp src/favicon.ico dist/favicon.ico
 npm run dev
---
+++ docker/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd ..
+docker stop rm-ticketin-test
+docker container prune --force
+docker image prune --force
+docker build --no-cache -t rm-ticketing-test .
+docker run --name=rm-ticketin-test -p 8085:80 -d rm-ticketing-test
+docker logs -f rm-ticketin-test
